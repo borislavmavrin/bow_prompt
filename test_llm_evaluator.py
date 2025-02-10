e65b00efd533e5835be43afd249ab3cd6868e101
Borislav Mavrin <borislav.mavrin@gmail.com>
2025-02-09 18:49:21 -0700

    feat: Add verbosity flag to experiments

--- test_llm_evaluator.py
+++ test_llm_evaluator.py
@@ -11,7 +11,8 @@ EPSILON = 0.5
 BEST_INSTRUCTION_LEN = 30
 INSTRUCTION_LEN = 50
 NUM_EVAL_EXAMPLES = 30
-NUM_STEPS = 30
+NUM_STEPS = 300
+VERBOSE = False
 # model_id = "qwen2.5:0.5b"
 # model_id = "llama3.2:1b"
 # model_id = "qwen2.5:1.5b"
@@ -25,17 +26,17 @@ boolq_task = get_boolq()
 
 learner_class_map = {
     "vanila": Learner,
-    "ucb": LearnerUCB,
-    "ridge": LearnerRidge
+    "ridge": LearnerRidge,
+    # "ucb": LearnerUCB
 }
-for modle_id in ["qwen2.5:0.5b", "llama3.2:1b", "qwen2.5:1.5b"]:
+for model_id in ["llama3.2:1b", "qwen2.5:1.5b"]:
     for learner_class in learner_class_map.keys():
         results_path = f"results/{learner_class}__{model_id.replace('/', '_')}"
-        assert not Path(results_path).exists()
+        assert not Path(results_path).exists(), str(results_path)
 
         ollama = Ollama(model_id)
-        evaluator = LLMEvaluator(provider=ollama, tasks=boolq_task[2:], seed=SEED, num_examples=NUM_EVAL_EXAMPLES)
-        learner = learner_class_map[learner_class](evaluator=evaluator, word_list=get_flan_vocab(), topk=BEST_INSTRUCTION_LEN, results_path=results_path)
+        evaluator = LLMEvaluator(provider=ollama, tasks=boolq_task[2:], seed=SEED, num_examples=NUM_EVAL_EXAMPLES, verbose=VERBOSE)
+        learner = learner_class_map[learner_class](evaluator=evaluator, word_list=get_flan_vocab(), topk=BEST_INSTRUCTION_LEN, results_path=results_path, verbose=VERBOSE)
         learner.run(steps=NUM_STEPS, num_idxs_to_choose=INSTRUCTION_LEN, epsilon=EPSILON)
         best_score, best_instruction = learner.evaluate_best_idxs()
         print(best_score, best_instruction)
